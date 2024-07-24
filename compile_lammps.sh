5290984b5d3fbcf246f1487c881f8b18da01c723
Luke45668 <luke.debono.21@ucl.ac.uk>
2024-07-24 13:02:44 +0100

    These scripts should compile

--- compile_lammps.sh
+++ compile_lammps.sh
@@ -1,8 +1,20 @@
+#!/bin/bash -l
 # execute this from the directory where you have downloaded lammps
-cd lammps-2Aug2023/src 
+cd lammps-2Aug2023 
+rm -rf test_build 
 mkdir test_build
+
 cd test_build 
+
+module unload mpi compilers gcc-libs 
+module load beta-modules 
+module load gcc-libs/10.2.0 
+module load compilers/intel/2022.2 
+module load mpi/intel/2019/update6/intel
+module load cmake/3.27.3 
+
 cmake  -D BUILD_MPI=yes  -D PKG_EXTRA-FIX=on -D PKG_EXTRA-PAIR=on -D PKG_KSPACE=on -D PKG_MANYBODY=on -D PKG_MOLECULE=on -D PKG_RIGID=on -D PKG_SRD=on -D PKG_EXTRA-COMPUTE=on ../cmake
+
 cmake --build . -j 4
 
 
--- test_job.sh
+++ test_job.sh
@@ -24,21 +24,21 @@
 # Select the MPI parallel environment and 36 processes.
 #$ -pe mpi 8
  
-#$ -wd /home/*INSERT_UCL_NAME*/Scratch/
+#$ -wd /home/*INSERT_UCL_USERNAME*/Scratch/
  
  
  echo "STARTING MYRIAD JOB"
  
  start=$(date +%s)
  
-#  module unload mpi compilers gcc-libs 
-#  module load beta-modules 
-#  module load gcc-libs/10.2.0 
-#  module load compilers/intel/2022.2 
-#  module load mpi/intel/2019/update6/intel 
-#  module load hdf/5-1.12.3-impi/intel-2022
+ module unload mpi compilers gcc-libs 
+ module load beta-modules 
+ module load gcc-libs/10.2.0 
+ module load compilers/intel/2022.2 
+ module load mpi/intel/2019/update6/intel 
+
  
- mpirun -np 8 home/*INSERT_UCL_NAME*/HPC_basic_run_test/lammps-2Aug2023/src/test_build/lmp  -in  home/*INSERT_UCL_NAME*/HPC_basic_run_test/in.srd.mixture
+ mpirun -np 8 home/*INSERT_UCL_USERNAME*/HPC_basic_run_test/lammps-2Aug2023/src/test_build/lmp  -in  home/*INSERT_UCL_USERNAME*/HPC_basic_run_test/in.srd.mixture
  
  wait
  
